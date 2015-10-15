77350bf31b05db4be1aa5bbe72456a47e306ee47
Den Drobiazko <den.d@techery.io>
2015-10-15 17:03:41 +0300

    Fixed a bug with xml-set rating was incremented at runtime. Changed tick's side margin to be actually padding under the hood - for bigger clickable area (and not only sides - top and bottom also). attr however left with 'margin' name - IMHO, for better understanding.

--- library/src/io/techery/properratingbar/ProperRatingBar.java
+++ library/src/io/techery/properratingbar/ProperRatingBar.java
@@ -8,7 +8,6 @@ import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -23,10 +22,10 @@ public class ProperRatingBar extends LinearLayout {
     private static final int DF_SYMBOLIC_TEXT_STYLE = Typeface.NORMAL;
     private static final int DF_SYMBOLIC_TEXT_NORMAL_COLOR = Color.BLACK;
     private static final int DF_SYMBOLIC_TEXT_SELECTED_COLOR = Color.GRAY;
-    private static final int DF_TICK_SIDE_MARGIN = 1;
+    private static final int DF_TICK_PADDING = 1;
 
     private int totalTicks;
-    private int selectedTicks;
+    private int lastSelectedTickIndex;
     private boolean isUsable;
     private String symbolicTick;
     private int customTextSize;
@@ -35,7 +34,7 @@ public class ProperRatingBar extends LinearLayout {
     private int customTextSelectedColor;
     private Drawable tickNormalDrawable;
     private Drawable tickSelectedDrawable;
-    private int tickSidePadding;
+    private int tickPadding;
     //
     private boolean useSymbolicTick = false;
     //
@@ -53,9 +52,11 @@ public class ProperRatingBar extends LinearLayout {
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ProperRatingBar);
         //
         totalTicks = a.getInt(R.styleable.ProperRatingBar_totalTicks, DF_TOTAL_TICKS);
-        selectedTicks = a.getInt(R.styleable.ProperRatingBar_selectedTicks, DF_SELECTED_TICKS);
-        rating = selectedTicks + 1;
+        rating = a.getInt(R.styleable.ProperRatingBar_selectedTicks, DF_SELECTED_TICKS);
+        if (rating > totalTicks) rating = totalTicks;
+        lastSelectedTickIndex = rating - 1;
         isUsable = a.getBoolean(R.styleable.ProperRatingBar_usable, DF_USABLE);
+        //
         symbolicTick = a.getString(R.styleable.ProperRatingBar_symbolicTick);
         if (symbolicTick == null) symbolicTick = context.getString(DF_SYMBOLIC_TICK);
         customTextSize = a.getDimensionPixelSize(R.styleable.ProperRatingBar_android_textSize,
@@ -66,7 +67,7 @@ public class ProperRatingBar extends LinearLayout {
         //
         tickNormalDrawable = a.getDrawable(R.styleable.ProperRatingBar_tickNormalDrawable);
         tickSelectedDrawable = a.getDrawable(R.styleable.ProperRatingBar_tickSelectedDrawable);
-        tickSidePadding = a.getDimensionPixelOffset(R.styleable.ProperRatingBar_tickSideMargin, DF_TICK_SIDE_MARGIN);
+        tickPadding = a.getDimensionPixelOffset(R.styleable.ProperRatingBar_tickMargin, DF_TICK_PADDING);
         //
         if (tickNormalDrawable == null || tickSelectedDrawable == null) {
             useSymbolicTick = true;
@@ -106,9 +107,7 @@ public class ProperRatingBar extends LinearLayout {
 
     private void addDrawableChild(Context context, int position) {
         ImageView iv = new ImageView(context);
-        LinearLayout.LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(tickSidePadding, 0, tickSidePadding, 0);
-        iv.setLayoutParams(layoutParams);
+        iv.setPadding(tickPadding, tickPadding, tickPadding, tickPadding);
         if (isUsable) {
             iv.setTag(R.id.prb_child_tag_id, position);
             iv.setOnClickListener(mTickClickedListener);
@@ -119,8 +118,8 @@ public class ProperRatingBar extends LinearLayout {
     private View.OnClickListener mTickClickedListener = new OnClickListener() {
         @Override
         public void onClick(View v) {
-            selectedTicks = (int) v.getTag(R.id.prb_child_tag_id);
-            rating = selectedTicks + 1;
+            lastSelectedTickIndex = (int) v.getTag(R.id.prb_child_tag_id);
+            rating = lastSelectedTickIndex + 1;
             redrawChildren();
             if (listener != null) listener.onRatePicked(rating);
         }
@@ -129,9 +128,9 @@ public class ProperRatingBar extends LinearLayout {
     private void redrawChildren() {
         for (int i = 0; i < totalTicks; i++) {
             if (useSymbolicTick) {
-                redrawChildSelection((TextView) ProperRatingBar.this.getChildAt(i), i <= selectedTicks);
+                redrawChildSelection((TextView) ProperRatingBar.this.getChildAt(i), i <= lastSelectedTickIndex);
             } else {
-                redrawChildSelection((ImageView) ProperRatingBar.this.getChildAt(i), i <= selectedTicks);
+                redrawChildSelection((ImageView) ProperRatingBar.this.getChildAt(i), i <= lastSelectedTickIndex);
             }
         }
     }
@@ -175,7 +174,7 @@ public class ProperRatingBar extends LinearLayout {
     public void setRating(int rating) {
         if (rating  > this.totalTicks) rating = totalTicks;
         this.rating = rating;
-        selectedTicks = rating - 1;
+        lastSelectedTickIndex = rating - 1;
         redrawChildren();
     }
 }
