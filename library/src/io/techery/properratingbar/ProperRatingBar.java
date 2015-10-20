ac91a5d3cfbc2cdb0949e24df9c80db1e8e44718
Den Drobiazko <den.d@techery.io>
2015-10-20 12:13:56 +0300

    Add a bit of documentation and licence.

--- library/src/io/techery/properratingbar/ProperRatingBar.java
+++ library/src/io/techery/properratingbar/ProperRatingBar.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
@@ -12,7 +13,32 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+/*
+    The MIT License (MIT)
+
+    Copyright (c) 2015 Techery (http://techery.io/)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+ */
+
 /**
+ * TODO : add doc
  * * See {@link R.styleable#ProperRatingBar ProperRatingBar Attributes}
  */
 public class ProperRatingBar extends LinearLayout {
@@ -41,7 +67,7 @@ public class ProperRatingBar extends LinearLayout {
 
     private boolean useSymbolicTick = false;
     private int rating;
-    private RatingListener listener;
+    private RatingListener listener = null;
 
     public ProperRatingBar(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -159,22 +185,42 @@ public class ProperRatingBar extends LinearLayout {
     // them getters and setters methods
     ///////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Get the attached {@link RatingListener}
+     * @return listener or null if none was set
+     */
+    @Nullable
     public RatingListener getListener() {
         return listener;
     }
 
+    /**
+     * Set the {@link RatingListener} to be called when user taps rating bar's ticks
+     * @param listener listener to set
+     */
     public void setListener(RatingListener listener) {
         this.listener = listener;
     }
 
+    /**
+     * Remove listener
+     */
     public void removeRatingListener() {
         this.listener = null;
     }
 
+    /**
+     * Get the current rating shown
+     * @return rating
+     */
     public int getRating() {
         return rating;
     }
 
+    /**
+     * Set the rating to show
+     * @param rating new rating value
+     */
     public void setRating(int rating) {
         if (rating  > this.totalTicks) rating = totalTicks;
         this.rating = rating;
